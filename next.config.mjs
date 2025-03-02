09301607db51354d29efbb4d22c1077ea1fc86e7
Andres <bdswtechstuff@gmail.com>
2025-03-02 17:14:38 -0600

    deploy(vercel)

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-    output: 'export',
-    basePath: isProd ? '/guzmandam.github.io' : '',
-    assetPrefix: isProd ? '/guzmandam.github.io/' : '',
-    images: {
-      unoptimized: true,
-    },
-  };
+const nextConfig = {};
 
 export default nextConfig;
